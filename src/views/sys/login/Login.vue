34a209b2da3691ec4c3fc702f8aa9551f2a8a806
linshan <1099013371@qq.com>
2021-03-24 22:02:52 +0800

    udate

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :class="prefixCls" class="relative w-full h-full px-4">
+  <div
+    :class="prefixCls"
+    class="relative w-full h-full px-4"
+  >
     <AppLocalePicker
       class="absolute top-4 right-4 enter-x text-white xl:text-gray-600"
       :showText="false"
@@ -28,9 +31,7 @@
           </div>
         </div>
         <div class="h-full xl:h-auto flex py-5 xl:py-0 xl:my-0 w-full xl:w-6/12">
-          <div
-            class="my-auto mx-auto xl:ml-20 bg-white xl:bg-transparent px-5 py-8 sm:px-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto enter-x relative"
-          >
+          <div class="my-auto mx-auto xl:ml-20 bg-white xl:bg-transparent px-5 py-8 sm:px-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto enter-x relative">
             <LoginForm />
             <ForgetPasswordForm />
             <RegisterForm />
@@ -43,139 +44,139 @@
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent, computed } from 'vue';
+import { defineComponent, computed } from 'vue';
 
-  import { AppLogo } from '/@/components/Application';
-  import { AppLocalePicker } from '/@/components/Application';
-  import LoginForm from './LoginForm.vue';
-  import ForgetPasswordForm from './ForgetPasswordForm.vue';
-  import RegisterForm from './RegisterForm.vue';
-  import MobileForm from './MobileForm.vue';
-  import QrCodeForm from './QrCodeForm.vue';
+import { AppLogo } from '/@/components/Application';
+import { AppLocalePicker } from '/@/components/Application';
+import LoginForm from './LoginForm.vue';
+import ForgetPasswordForm from './ForgetPasswordForm.vue';
+import RegisterForm from './RegisterForm.vue';
+import MobileForm from './MobileForm.vue';
+import QrCodeForm from './QrCodeForm.vue';
 
-  import { useGlobSetting } from '/@/hooks/setting';
-  import { useI18n } from '/@/hooks/web/useI18n';
-  import { useDesign } from '/@/hooks/web/useDesign';
-  import { localeStore } from '/@/store/modules/locale';
+import { useGlobSetting } from '/@/hooks/setting';
+import { useI18n } from '/@/hooks/web/useI18n';
+import { useDesign } from '/@/hooks/web/useDesign';
+import { localeStore } from '/@/store/modules/locale';
 
-  export default defineComponent({
-    name: 'Login',
-    components: {
-      AppLogo,
-      LoginForm,
-      ForgetPasswordForm,
-      RegisterForm,
-      MobileForm,
-      QrCodeForm,
-      AppLocalePicker,
-    },
-    setup() {
-      const globSetting = useGlobSetting();
-      const { prefixCls } = useDesign('login');
-      const { t } = useI18n();
+export default defineComponent({
+  name: 'Login',
+  components: {
+    AppLogo,
+    LoginForm,
+    ForgetPasswordForm,
+    RegisterForm,
+    MobileForm,
+    QrCodeForm,
+    AppLocalePicker,
+  },
+  setup() {
+    const globSetting = useGlobSetting();
+    const { prefixCls } = useDesign('login');
+    const { t } = useI18n();
 
-      return {
-        t,
-        prefixCls,
-        title: computed(() => globSetting?.title ?? ''),
-        showLocale: localeStore.getShowPicker,
-      };
-    },
-  });
+    return {
+      t,
+      prefixCls,
+      title: computed(() => globSetting?.title ?? ''),
+      showLocale: localeStore.getShowPicker,
+    };
+  },
+});
 </script>
 <style lang="less">
-  @prefix-cls: ~'@{namespace}-login';
-  @logo-prefix-cls: ~'@{namespace}-app-logo';
-  @countdown-prefix-cls: ~'@{namespace}-countdown-input';
+@prefix-cls: ~'@{namespace}-login';
+@logo-prefix-cls: ~'@{namespace}-app-logo';
+@countdown-prefix-cls: ~'@{namespace}-countdown-input';
 
-  .@{prefix-cls} {
+.@{prefix-cls} {
+  @media (max-width: @screen-xl) {
+    background: linear-gradient(180deg, #1c3faa, #1c3faa);
+  }
+
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin-left: -48%;
+    background-image: url(/@/assets/svg/login-bg.svg);
+    background-position: 100%;
+    background-repeat: no-repeat;
+    background-size: auto 100%;
+    content: '';
     @media (max-width: @screen-xl) {
-      background: linear-gradient(180deg, #1c3faa, #1c3faa);
+      display: none;
+    }
+  }
+
+  .@{logo-prefix-cls} {
+    position: absolute;
+    top: 12px;
+    height: 30px;
+
+    &__title {
+      font-size: 16px;
+      color: #fff;
     }
 
-    &::before {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      margin-left: -48%;
-      background-image: url(/@/assets/svg/login-bg.svg);
-      background-position: 100%;
-      background-repeat: no-repeat;
-      background-size: auto 100%;
-      content: '';
-      @media (max-width: @screen-xl) {
-        display: none;
-      }
+    img {
+      width: 32px;
     }
+  }
 
+  .container {
     .@{logo-prefix-cls} {
-      position: absolute;
-      top: 12px;
-      height: 30px;
+      display: flex;
+      width: 60%;
+      height: 80px;
 
       &__title {
-        font-size: 16px;
+        font-size: 24px;
         color: #fff;
       }
 
       img {
-        width: 32px;
+        width: 48px;
       }
-    }
-
-    .container {
-      .@{logo-prefix-cls} {
-        display: flex;
-        width: 60%;
-        height: 80px;
-
-        &__title {
-          font-size: 24px;
-          color: #fff;
-        }
-
-        img {
-          width: 48px;
-        }
-      }
-    }
-
-    &-sign-in-way {
-      .anticon {
-        font-size: 22px;
-        color: #888;
-        cursor: pointer;
-
-        &:hover {
-          color: @primary-color;
-        }
-      }
-    }
-
-    input:not([type='checkbox']) {
-      min-width: 360px;
-
-      @media (max-width: @screen-lg) {
-        min-width: 300px;
-      }
-
-      @media (max-width: @screen-md) {
-        min-width: 280px;
-      }
-
-      @media (max-width: @screen-sm) {
-        min-width: 180px;
-      }
-    }
-    .@{countdown-prefix-cls} input {
-      min-width: unset;
-    }
-
-    .ant-divider-inner-text {
-      font-size: 12px;
-      color: @text-color-secondary;
     }
   }
+
+  &-sign-in-way {
+    .anticon {
+      font-size: 22px;
+      color: #888;
+      cursor: pointer;
+
+      &:hover {
+        color: @primary-color;
+      }
+    }
+  }
+
+  input:not([type='checkbox']) {
+    min-width: 360px;
+
+    @media (max-width: @screen-lg) {
+      min-width: 300px;
+    }
+
+    @media (max-width: @screen-md) {
+      min-width: 280px;
+    }
+
+    @media (max-width: @screen-sm) {
+      min-width: 180px;
+    }
+  }
+  .@{countdown-prefix-cls} input {
+    min-width: unset;
+  }
+
+  .ant-divider-inner-text {
+    font-size: 12px;
+    color: @text-color-secondary;
+  }
+}
 </style>
